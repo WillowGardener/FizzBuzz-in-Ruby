62e500a3f98fd06dd6669a56f91dee4fd2dce64c
griffin wimminger & Scott Dudley <student@epicodus.com>
2014-07-21 14:28:02 -0700

    Fully working fizzbuzz, including some refactoring

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,18 @@
 def fizzbuzz(number)
-  if number % 3 == 0
-    "fizz"
+
+  result = ""
+
+  1.upto(number) do |digit|
+    if digit % 5 == 0 && digit % 3 == 0
+      result += "fizzbuzz "
+    elsif digit % 3 == 0
+      result += "fizz "
+    elsif digit % 5 == 0
+      result += "buzz "
+    else
+      result += digit.to_s + " "
+    end
   end
+
+  result.strip
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,6 +3,12 @@ require('fizzbuzz')
 
 describe("fizzbuzz") do
   it("any number divisible by 3 is replaced by 'fizz'") do
-    fizzbuzz(3).should(eq("fizz"))
+    fizzbuzz(3).should(eq("1 2 fizz"))
+  end
+  it("any number divisible by 5 is replaced by 'buzz'") do
+    fizzbuzz(5).should(eq("1 2 fizz 4 buzz"))
+  end
+  it("any number divisible by both 3 and 5 is replaced by 'fizzbuzz'") do
+    fizzbuzz(15).should(eq("1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz"))
   end
 end
